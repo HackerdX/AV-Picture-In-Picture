0e8e66a7855df0860d6413a14e860c49fc5ad595
HackerdX <cooleasycodebyanmol@gmail.com>
2021-03-02 12:18:45 +0530

    Fixed Button Fuctionality for PiP

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
 const videoElement = document.getElementById('video');
-const button = document.getElementById('button')
+const button = document.getElementById('button');
+let checker = document.getElementById('checker');
 // Prompt to select media screen, pass the video element, then play
 async function selectMediaStream(){
     try{
         const mediaStream = await navigator.mediaDevices.getDisplayMedia(); /* This will create a prompt to share your screen for different applications, we need only a google tab having a video enabled*/
-
-        console.log(mediaStream);   
         videoElement.srcObject = mediaStream;
-        videoElement.onloadedmetadata = () => {
-            videoElement.play();
-        }
-
+        videoElement.play();
     } catch(error){
         console.log("something went wrong: ", error);
     }
  }
 
- button.addEventListener('click', async () => {
-    // Disable button
-    button.disable = true;
-
-    // Start Picture in Picture
-    await videoElement.requestPictureInPicture();
-
-    // Enable button
-    button.disable = false;
- })
+ if('pictureInPictureEnabled' in document){
+    button.addEventListener('click', async () => {
+        if(document.pictureInPictureElement){
+            button.disable = false;
+            document.exitPictureInPicture().catch(e => console.log(e))
+        } else{
+            button.disable = true;
+            video.requestPictureInPicture().catch(e => console.log(e))
+        }
+    })
+}
 
 //On Load
 selectMediaStream();
